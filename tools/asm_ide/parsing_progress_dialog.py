92a365fc0f8e58ad23ff5171b49abec38c0c209d
Michael Nix <mchl.nix@googlemail.com>
2025-02-09 15:48:42 +0900

    make sure progress dialog gets shown correctly

--- tools/asm_ide/parsing_progress_dialog.py
+++ tools/asm_ide/parsing_progress_dialog.py
@@ -1,5 +1,5 @@
 from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QProgressDialog
+from PySide6.QtWidgets import QApplication, QProgressDialog
 
 
 class ParsingProgressDialog(QProgressDialog):
@@ -16,6 +16,10 @@ class ParsingProgressDialog(QProgressDialog):
         self.setWindowModality(Qt.ApplicationModal)
 
         self.show()
+
+        # need (at least) two calls to have the dialog show up correctly on fast PCs
+        QApplication.processEvents()
+        QApplication.processEvents()
 
         for index, file_being_parsed in enumerate(self.parsing_generator, 1):
             self.setValue(index)
