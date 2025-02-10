e3a835f7ad9a2557831e1049c180df98e568e38e
Michael Nix <mchl.nix@googlemail.com>
2025-02-10 17:06:31 +0900

    adds line number display (might need cleanup)

--- tools/asm_ide/code_area.py
+++ tools/asm_ide/code_area.py
@@ -1,15 +1,93 @@
+from math import floor, log10
 from pathlib import Path
 
-from PySide6.QtCore import Signal
-from PySide6.QtGui import QFont, QMouseEvent, Qt, QTextBlock, QTextCursor, QTextDocument
-from PySide6.QtWidgets import QTextEdit, QToolTip
+from PySide6.QtCore import QPoint, QRect, QSize, Signal
+from PySide6.QtGui import (
+    QColor,
+    QFont,
+    QFontMetrics,
+    QMouseEvent,
+    QPainter,
+    QPaintEvent,
+    Qt,
+    QTextBlock,
+    QTextCursor,
+    QTextDocument,
+)
+from PySide6.QtWidgets import (
+    QPlainTextDocumentLayout,
+    QPlainTextEdit,
+    QToolTip,
+    QWidget,
+)
 
 from foundry import ctrl_is_pressed
 from tools.asm_ide.asm_syntax_highlighter import AsmSyntaxHighlighter
 from tools.asm_ide.named_value_finder import NamedValueFinder
 
 
-class CodeArea(QTextEdit):
+class LineNumberArea(QWidget):
+    MARGIN_RIGHT = 10
+
+    def __init__(self, editor: "CodeArea"):
+        super().__init__(editor)
+
+        self.editor = editor
+
+        self._line_no_width = self._line_no_height = 1
+
+    def update_text_measurements(self):
+        font_metrics = QFontMetrics(self.editor.document().defaultFont())
+        self._line_no_width = font_metrics.horizontalAdvance(self.no_of_digits * "9")
+        print(
+            font_metrics.lineSpacing(),
+            font_metrics.height(),
+            font_metrics.leading(),
+            font_metrics.boundingRect("Gg").height(),
+        )
+        self._line_no_height = font_metrics.lineSpacing()
+
+        self.editor.setViewportMargins(self.sizeHint().width(), 0, 0, 0)
+
+    @property
+    def no_of_digits(self):
+        line_count = self.editor.document().lineCount()
+        digits_in_last_line_no = floor(log10(line_count) + 1)
+
+        return digits_in_last_line_no
+
+    def sizeHint(self):
+        return QSize(self._line_no_width + self.MARGIN_RIGHT, self.editor.maximumSize().height())
+
+    def paintEvent(self, event: QPaintEvent):
+        self.paint_area(event)
+
+    def paint_area(self, paint_event: QPaintEvent):
+        painter = QPainter(self)
+        painter.setFont(self.editor.font)
+        painter.setPen(QColor(150, 150, 150))
+
+        block = self.editor.firstVisibleBlock()
+        top = self.editor.blockBoundingGeometry(block).translated(self.editor.contentOffset()).top() + 1
+        bottom = top + self.editor.blockBoundingRect(block).height()
+
+        rect = QRect(0, top, self._line_no_width, bottom)
+
+        while block.isValid() and block.isVisible():
+            line_no_str = str(block.blockNumber() + 1)
+
+            painter.drawText(rect, Qt.AlignmentFlag.AlignBaseline | Qt.AlignmentFlag.AlignRight, line_no_str)
+
+            rect.adjust(0, self._line_no_height + 1, 0, self._line_no_height + 1)
+
+            block = block.next()
+
+        painter.drawLine(QPoint(self.width() - 1, 0), QPoint(self.width() - 1, self.height()))
+
+        painter.end()
+
+
+class CodeArea(QPlainTextEdit):
     text_position_clicked = Signal(int)
     """
     :param The block index the cursor was put to.
@@ -24,18 +102,27 @@ class CodeArea(QTextEdit):
         super(CodeArea, self).__init__(parent)
 
         self._named_value_finder = named_value_finder
-        self.setLineWrapMode(QTextEdit.LineWrapMode.NoWrap)
+        self.setLineWrapMode(QPlainTextEdit.LineWrapMode.NoWrap)
 
         self.text_document = QTextDocument()
+        self.text_document.setDocumentLayout(QPlainTextDocumentLayout(self.text_document))
         self.setDocument(self.text_document)
 
         self.syntax_highlighter = AsmSyntaxHighlighter(self, named_value_finder)
         self.syntax_highlighter.setDocument(self.text_document)
 
-        self.text_document.setDefaultFont(QFont("Monospace", 14))
+        self.font = QFont("Monospace", 14)
+        self.text_document.setDefaultFont(self.font)
 
         self.last_block: QTextBlock | None = None
         self.last_word = ""
+
+        self._line_number_area = LineNumberArea(self)
+        self.blockCountChanged.connect(self._line_number_area.update_text_measurements)
+
+    def paintEvent(self, e: QPaintEvent):
+        self._line_number_area.repaint()
+        return super().paintEvent(e)
 
     def mouseMoveEvent(self, e):
         text_cursor = self.cursorForPosition(e.pos())
@@ -55,6 +142,7 @@ class CodeArea(QTextEdit):
             self.syntax_highlighter.rehighlightBlock(self.last_block)
             self.last_block = None
 
+        # todo: dedup
         if word in self._named_value_finder.constants:
             self.last_word = word
 
@@ -93,6 +181,7 @@ class CodeArea(QTextEdit):
         navigated_by_mouse = event.buttons() & (Qt.MouseButton.BackButton | Qt.MouseButton.ForwardButton)
 
         if cursor_pos_changed and not navigated_by_mouse:
+            # todo: could be done with signal of QPlainTextEdit
             self.text_position_clicked.emit(text_cursor_at_click.position())
 
         return super().mousePressEvent(event)
