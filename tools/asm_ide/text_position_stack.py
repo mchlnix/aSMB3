41a367d8ffce912d10f6bfa1ae225d79407d5009
Michael Nix <mchl.nix@googlemail.com>
2025-02-16 19:23:30 +0900

    drop text position at start of file, when next position is in same file

--- tools/asm_ide/text_position_stack.py
+++ tools/asm_ide/text_position_stack.py
@@ -1,24 +1,48 @@
 from pathlib import Path
+from typing import NamedTuple
+
+
+class TextPosition(NamedTuple):
+    file_path: Path
+    pos_in_text: int
 
 
 class TextPositionStack:
     def __init__(self):
-        self.stack: list[tuple[Path, int]] = []
+        self.stack: list[TextPosition] = []
 
         self.current_index = -1
+
+        self.overwrite_position_at_top_of_the_file = True
+        """
+        When a new position is pushed for a file, and the last position on the stack was for the same file at position
+        0, so at the beginning of the file, then drop that one and overwrite it with the given position.
+
+        Idea behind that is, that when a new file is opened, it make sense to add a position there, but as soon as the
+        user sets one themselves, it's bothersome to have this unintended position in the stack.
+        """
 
     def push(self, file_path: Path, pos_in_text: int):
         self.truncate()
 
-        self.stack.append((file_path, pos_in_text))
+        if self.overwrite_position_at_top_of_the_file and self.stack:
+            head = self.stack[self.current_index]
+
+            if head.file_path == file_path and head.pos_in_text == 0:
+                self.go_back()
+                self.push(file_path, pos_in_text)
+
+                return
+
+        self.stack.append(TextPosition(file_path, pos_in_text))
         self.current_index += 1
 
-    def go_forward(self) -> tuple[Path, int]:
+    def go_forward(self) -> TextPosition:
         self.current_index += 1
 
         return self.stack[self.current_index]
 
-    def go_back(self) -> tuple[Path, int]:
+    def go_back(self) -> TextPosition:
         self.current_index -= 1
 
         return self.stack[self.current_index]
