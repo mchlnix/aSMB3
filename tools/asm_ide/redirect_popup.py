6f7c91a563ee93a821e69d0c9bbc2a05a58f184a
Michael Nix <mchl.nix@googlemail.com>
2025-02-16 17:59:33 +0900

    clean up and style redirect popup

--- tools/asm_ide/redirect_popup.py
+++ tools/asm_ide/redirect_popup.py
@@ -12,10 +12,17 @@ from PySide6.QtWidgets import (
 from tools.asm_ide.reference_finder import ReferenceDefinition, ReferenceType
 
 
+def _smb3_first_sort_key(reference: ReferenceDefinition):
+    if reference.origin_file.name == "smb3.asm":
+        return ReferenceDefinition("", "", "a", 0, ReferenceType.UNSET, "")
+    else:
+        return reference
+
+
 class RedirectPopup(QWidget):
     def __init__(self, definition: ReferenceDefinition, references: list[ReferenceDefinition], parent):
         super(RedirectPopup, self).__init__(parent)
-
+        self.setContentsMargins(0, 0, 0, 0)
         self.setAutoFillBackground(True)
 
         self._layout = QHBoxLayout()
@@ -30,75 +37,105 @@ class ReferenceTableWidget(QTableWidget):
     def __init__(self, definition: ReferenceDefinition, references: list[ReferenceDefinition], parent=None):
         super(ReferenceTableWidget, self).__init__(parent)
 
-        self.setWindowModality(Qt.WindowModality.WindowModal)
-        self.setAutoFillBackground(True)
+        self._setup_widget()
 
-        self.file_font = self.font()
+        self._setup_table(references)
 
-        self.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Maximum)
+        self._bold_font = self.font()
+        self._bold_font.setBold(True)
 
-        self.setSelectionBehavior(self.SelectionBehavior.SelectRows)
-        self.setSelectionMode(self.SelectionMode.SingleSelection)
+        self._next_row_index = 0
 
-        self.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeMode.ResizeToContents)
-        self.horizontalHeader().setStretchLastSection(True)
-
-        self.setFont(QFont("Monospace", 12))
-
-        self.setRowCount(len(references))
-        self.setColumnCount(3)
-
-        file_item = QTableWidgetItem(str(definition.origin_file))
-        file_item.setForeground(QBrush(QColor.fromRgb(0xA626A4)))
-
-        font = file_item.font()
-        font.setBold(True)
-        file_item.setFont(font)
-
-        line_number_item = QTableWidgetItem(str(definition.origin_line_no))
-        line_number_item.setForeground(QBrush(QColor(0, 0, 255)))
-        line_number_item.setTextAlignment(Qt.AlignmentFlag.AlignRight | Qt.AlignmentFlag.AlignVCenter)
-
-        self._add_row(0, file_item, line_number_item, QTableWidgetItem(definition.line))
-
+        self._add_definition_row(definition)
         self._add_reference_rows(references)
 
         self.setFocus()
 
+        self.resizeRowsToContents()
+
+    def _setup_widget(self):
+        self.setWindowModality(Qt.WindowModality.WindowModal)
+        self.setAutoFillBackground(True)
+
+        self.setSizePolicy(QSizePolicy.Policy.Maximum, QSizePolicy.Policy.Maximum)
+        self.setFont(QFont("Monospace", 14))
+
+    def _setup_table(self, references: list[ReferenceDefinition]):
+        self.setSelectionBehavior(self.SelectionBehavior.SelectRows)
+        self.setSelectionMode(self.SelectionMode.SingleSelection)
+
+        self.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeMode.ResizeToContents)
         self.horizontalHeader().setVisible(False)
         self.verticalHeader().setVisible(False)
 
-        self.resizeRowsToContents()
+        # make readonly
+        self.setEditTriggers(QTableWidget.EditTrigger.NoEditTriggers)
 
-        self.setSizePolicy(QSizePolicy.Policy.Maximum, QSizePolicy.Policy.Maximum)
+        # set row count
+        row_count = 1 + 1  # "Definitions" row and the actual definition
+
+        if references:
+            row_count += 1 + len(references)  # "References" row and references
+
+        self.setRowCount(row_count)
+        self.setColumnCount(3)
+
+    def _add_label_row(self, label_text: str):
+        label_item = QTableWidgetItem(label_text)
+        label_item.setForeground(QColor.fromRgb(0xA0A1A7))
+
+        self._add_row(label_item)
+
+    def _add_definition_row(self, definition):
+        self._add_label_row("Definition:")
+
+        file_item = QTableWidgetItem(str(definition.origin_file))
+
+        file_item.setForeground(QBrush(QColor.fromRgb(0xA626A4)))
+        file_item.setFont(self._bold_font)
+
+        line_number_item = QTableWidgetItem(str(definition.origin_line_no))
+
+        line_number_item.setForeground(QBrush(QColor.fromRgb(0x2C91AF)))
+        line_number_item.setFont(self._bold_font)
+        line_number_item.setTextAlignment(Qt.AlignmentFlag.AlignRight | Qt.AlignmentFlag.AlignVCenter)
+
+        self._add_row(file_item, line_number_item, QTableWidgetItem(definition.line))
 
     def _add_reference_rows(self, references):
-        def _smb3_first(_reference: ReferenceDefinition):
-            if _reference.origin_file.name == "smb3.asm":
-                return ReferenceDefinition("", "", "a", 0, ReferenceType.UNSET, "")
-            else:
-                return _reference
+        if not references:
+            return
+
+        self._add_label_row("References:")
 
         last_file = ""
 
-        for row, reference in enumerate(sorted(references, key=_smb3_first), 1):
+        for reference in sorted(references, key=_smb3_first_sort_key):
             file_item = QTableWidgetItem(str(reference.origin_file))
-
-            if last_file != reference.origin_file:
-                font = file_item.font()
-                font.setBold(True)
-                file_item.setFont(font)
-
-                last_file = reference.origin_file
+            file_item.setForeground(QBrush(QColor.fromRgb(0xA626A4)))
 
             line_number_item = QTableWidgetItem(str(reference.origin_line_no))
+            line_number_item.setForeground(QBrush(QColor.fromRgb(0x2C91AF)))
             line_number_item.setTextAlignment(Qt.AlignmentFlag.AlignRight | Qt.AlignmentFlag.AlignVCenter)
 
-            self._add_row(row, file_item, line_number_item, QTableWidgetItem(reference.line))
+            if last_file != reference.origin_file:
+                last_file = reference.origin_file
 
-    def _add_row(self, row: int, *cells: QTableWidgetItem):
+                file_item.setFont(self._bold_font)
+                line_number_item.setFont(self._bold_font)
+
+            self._add_row(file_item, line_number_item, QTableWidgetItem(reference.line))
+
+    def _add_row(self, *cells: QTableWidgetItem | str):
         for column, cell in enumerate(cells):
-            self.setItem(row, column, cell)
+            if isinstance(cell, str):
+                cell = QTableWidgetItem(cell)
+
+            self.setItem(self._next_row_index, column, cell)
+
+        self._next_row_index += 1
 
     def sizeHint(self):
-        return QSize(self.horizontalHeader().width(), self.verticalHeader().height())
+        width = sum(self.columnWidth(column_index) for column_index in range(self.columnCount() + 1)) + 2
+
+        return QSize(width, self.rowCount() * self.rowHeight(0) + 2)
