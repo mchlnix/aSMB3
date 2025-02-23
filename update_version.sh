4269321968131d8395f9882185660ca7f4d08cc2
Michael Nix <mchl.nix@googlemail.com>
2025-02-24 03:48:43 +0900

    adds new update version script

---
+++ update_version.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+CURRENT_VERSION=$(cat VERSION)
+
+read -p "Current version is $CURRENT_VERSION. Input new version: " NEW_VERSION
+
+if [ "$CURRENT_VERSION" == "$NEW_VERSION" ]; then
+  echo "New version is the same as the old one."
+  exit 1
+fi
+
+echo "Updating VERSION file. (Press Enter)"
+read
+echo "$NEW_VERSION" > VERSION
+
+echo "Adding VERSION file and making a version bump commit. (Press Enter)"
+read
+git add VERSION
+git commit -m "version bump $NEW_VERSION"
+
+echo
+echo "Making an annotated tag. First line should be '$NEW_VERSION Some-Code-Name' then a new line and change notes. (Press Enter)"
+read
+git tag -a "$NEW_VERSION"
+
+echo "Pushing changes and new release to github. (Press Enter)"
+read
+git push --tags
