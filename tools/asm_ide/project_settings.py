4aeab4b4733e7dbfd62416e6dad4deebb6931613
Michael Nix <mchl.nix@googlemail.com>
2025-02-26 03:43:25 +0900

    add helper methods to project settings

--- tools/asm_ide/project_settings.py
+++ tools/asm_ide/project_settings.py
@@ -6,14 +6,16 @@ from typing import Any
 
 class ProjectSettingKeys(StrEnum):
     HIGHLIGHTED_LINE_NUMBERS = "highlight_line_numbers"
+    LAST_POSITION_IN_FILES = "last_position_in_files"
     OPEN_FILES = "open_files"
     OPEN_TAB_INDEX = " open_tab_index"
 
 
 _DEFAULT_VALUES: dict[ProjectSettingKeys, dict | list | int] = {
-    ProjectSettingKeys.HIGHLIGHTED_LINE_NUMBERS: {},
-    ProjectSettingKeys.OPEN_FILES: [],
-    ProjectSettingKeys.OPEN_TAB_INDEX: -1,
+    ProjectSettingKeys.HIGHLIGHTED_LINE_NUMBERS: {},  # dict[rel_path_str, list[line_no]]
+    ProjectSettingKeys.LAST_POSITION_IN_FILES: {},  # dict[rel_path_str, tuple[text_pos, scroll_pos]]
+    ProjectSettingKeys.OPEN_FILES: [],  # list[rel_path_str]
+    ProjectSettingKeys.OPEN_TAB_INDEX: -1,  # tab_index
 }
 
 
@@ -41,4 +43,29 @@ class ProjectSettings:
         return self._settings[key]
 
     def sync(self):
-        self._save_path.write_text(json.dumps(self._settings))
+        self._save_path.write_text(json.dumps(self._settings, indent=2))
+
+    def clear_open_files(self):
+        self.set_value(ProjectSettingKeys.OPEN_FILES, [])
+
+    def add_open_file(self, abs_path: Path):
+        currently_open_files = self.value(ProjectSettingKeys.OPEN_FILES)
+
+        currently_open_files.append(self._to_rel_path_str(abs_path))
+
+        self.set_value(ProjectSettingKeys.OPEN_FILES, currently_open_files)
+
+    def save_position_in_file(self, abs_path: Path, text_position: int, scroll_position: int):
+        last_position_in_files: dict[str, tuple[int, int]] = self.value(ProjectSettingKeys.LAST_POSITION_IN_FILES)
+
+        last_position_in_files[self._to_rel_path_str(abs_path)] = text_position, scroll_position
+
+        self.set_value(ProjectSettingKeys.LAST_POSITION_IN_FILES, last_position_in_files)
+
+    def load_position_in_file(self, abs_path: Path) -> tuple[int, int]:
+        last_position_in_files: dict[str, tuple[int, int]] = self.value(ProjectSettingKeys.LAST_POSITION_IN_FILES)
+
+        return last_position_in_files.get(self._to_rel_path_str(abs_path), (0, 0))
+
+    def _to_rel_path_str(self, abs_path: Path):
+        return str(abs_path.relative_to(self.root_path))
