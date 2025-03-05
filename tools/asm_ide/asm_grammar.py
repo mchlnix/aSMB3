3f728021f0852cdc2a32204b4eaddc34a142abec
Michael Nix <mchl.nix@googlemail.com>
2025-03-06 00:47:01 +0900

    first working grammar

--- tools/asm_ide/asm_grammar.py
+++ tools/asm_ide/asm_grammar.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from time import time
 
 from parsimonious.grammar import Grammar
 
@@ -17,7 +18,7 @@ asm_grammar = Grammar(
 
     empty_line            = newline
     comment_line          = ws? comment newline
-    line                  = ws? (symbol_def / orphaned_bytes / orphaned_words / directive_use / instruction_use) comment? newline
+    line                  = ws? (symbol_def / orphaned_bytes / orphaned_words / directive_use / instruction_use / macro_use) comment? newline
 
     ws                    = ~r"\\h*"
     newline               = ws? ("\\r\\n" / "\\n")
@@ -25,7 +26,7 @@ asm_grammar = Grammar(
     comment               = ws? ";" ws? everything?
     everything            = ~".*"
 
-    instruction_use       = instruction (ws ("#" / "<")? "~"? (expression / indirect_address) (ws? "," ws? register)?)?
+    instruction_use       = instruction (ws ("#" / "<")? (expression / indirect_address) (ws? "," ws? register)?)?
     indirect_address      = "[" symbol "]"
     instruction           = {" / ".join(QUOTED_INSTRUCTIONS)} / {" / ".join(map(str.lower, QUOTED_INSTRUCTIONS))} / unknown_symbol
     register              = "X" / "Y"
@@ -41,11 +42,11 @@ asm_grammar = Grammar(
     orphaned_words        = dot_word
 
     dot_ds                = ws? ".ds" ws expression
-    dot_include           = ws? ".include" ws string_literal
+    dot_include           = (symbol ":")? ws? ".include" ws string_literal
     dot_incchr            = ws? ".incchr" ws string_literal
     dot_org               = ws? ".org" ws symbol
 
-    symbol_def            = macro_def / array_def / function_def / ram_var_def / local_var_def / label_def / const_def
+    symbol_def            = macro_def / array_def / function_def / ram_var_def / local_var_def / dot_include / label_def / const_def
 
     macro_def             = macro_start (macro_body+ / if_clause) macro_end
     macro_start           = ws? symbol ":"? ws ".macro" comment? newline
@@ -54,15 +55,17 @@ asm_grammar = Grammar(
     macro_instruction_use = (instruction ws macro_parameter) / instruction_use
     macro_end             = ws? (".endm" / ".ENDM")
 
+    macro_use            = symbol ws expression_list
+
     if_clause             = if_start if_body+ if_end newline
     if_start              = ws? "if" ws (macro_parameter / literal) ws comparator ws (macro_parameter / literal) comment? newline
     comparator            = "<" / ">"
     if_body               = ws? "fail" everything newline
     if_end                = ws? "endif"
 
-    function_def          = symbol ws ".func" ws expression    
+    function_def          = symbol ":"? ws ".func" ws expression
     ram_var_def           = repeating_symbols ws? symbol ":" ws ".ds" ws expression
-    label_def             = ws? symbol ":" (ws? instruction_use)?
+    label_def             = ws? symbol ":" ws? (macro_use / instruction_use)?
     local_var_def         = ws? local_symbol ":" ws? everything
     const_def             = symbol ws? "=" ws? expression
 
@@ -79,12 +82,12 @@ asm_grammar = Grammar(
 
     symbol                = global_symbol / local_symbol
 
-    global_symbol         = ~"[A-Za-z][A-Za-z0-9_]*"
+    global_symbol         = ~"[A-Za-z_][A-Za-z0-9_]*"
     local_symbol          = "." global_symbol
 
     literal               = string_literal / number_literal
 
-    string_literal        = ~r"\\".*\\""
+    string_literal        = ~r"\\".*?\\""
     number_literal        = negation? (hex_literal / bin_literal / dec_literal)
 
     negation              = "-"
@@ -96,9 +99,9 @@ asm_grammar = Grammar(
     dec_digit             = ~"[0-9]"
     bin_digit             = "0" / "1"
 
-    function_call         = symbol "(" expression ")"
+    function_call         = symbol "(" ws? expression_list ws? ")"
 
-    expression            = (paren_group / number_literal / macro_parameter / function_call / symbol) (ws operator ws expression)?
+    expression            = negation? bit_negation? (paren_group / literal / macro_parameter / function_call / symbol) (ws operator ws expression)?
     operator              = "&" / "<<" / ">>" / "+" / "-" / "*" / "|" / "/"
     paren_group           = "(" expression ")"
 
@@ -106,12 +109,16 @@ asm_grammar = Grammar(
     """
 )
 
-path = Path("/home/michael/Gits/smb3/smb3.asm")
+if __name__ == "__main__":
+    start = time()
+    path = Path("/home/michael/Gits/smb3/smb3.asm")
 
-print(path)
-asm_grammar.parse(path.read_text())
-
-for i in range(32):
-    path = Path(f"/home/michael/Gits/smb3/PRG/prg{i:0>3}.asm")
     print(path)
     asm_grammar.parse(path.read_text())
+
+    for i in range(32):
+        path = Path(f"/home/michael/Gits/smb3/PRG/prg{i:0>3}.asm")
+        print(path)
+        asm_grammar.parse(path.read_text())
+
+    print(time() - start)
